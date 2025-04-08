565b5290c6c67e9a755caaa8a147f96d9a358e10
insung52 <justheare0502@gmail.com>
2025-04-08 22:16:17 +0900

    initial commit

---
+++ Source/vrfinal/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class VRFINAL_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
